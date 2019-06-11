b53bba5da4fc686f0af6ddb566dc11d08ec94b32
Edward Iga Kigongo <igakigongo@gmail.com>
2019-06-11 12:28:50 +0200

    Add method stubs for enumerable module

--- enumarable.rb
+++ enumarable.rb
@@ -2,4 +2,22 @@
 
 # Enumerable Module
 module Enumerable
+  # my_all? (continue as above)
+  def my_all?; end
+
+  def my_any?; end
+
+  def my_count; end
+
+  def my_each; end
+
+  def my_each_with_index; end
+
+  def my_inject; end
+
+  def my_map; end
+
+  def my_none?; end
+
+  def my_select; end
 end
