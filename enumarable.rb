8bd7b4e8521baf00d55047849f03e9916600f2dd
Edward Iga Kigongo <igakigongo@gmail.com>
2019-06-11 16:04:55 +0200

    Added Enumerable.my_each

--- enumarable.rb
+++ enumarable.rb
@@ -9,7 +9,16 @@ module Enumerable
 
   def my_count; end
 
-  def my_each; end
+  # provides an enumeration over items,
+  # if a block is given - it will be applied to each element
+  # befor returning the enumeration
+  def my_each(&prc)
+    return to_enum(:my_each) unless block_given?
+
+    index = 0
+    prc.call(self[index]) && index += 1 until index.equal?(length)
+    self
+  end
 
   def my_each_with_index; end
 
@@ -20,4 +29,10 @@ module Enumerable
   def my_none?; end
 
   def my_select; end
+end
+
+# manual tests - my_each
+p %w[edward was here on sunday].my_each
+%w[edward was here on sunday].my_each do |ele|
+  p ele * 1
 end
