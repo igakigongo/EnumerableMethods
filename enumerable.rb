7e5122941e8830539a44bb759362036653eb0a38
Edward Iga Kigongo <igakigongo@gmail.com>
2019-06-12 22:01:58 +0200

    Complete enumerable methods

--- enumerable.rb
+++ enumerable.rb
@@ -7,7 +7,7 @@ module Enumerable
   def my_all?(pattern = nil)
     result = true
     if block_given?
-      my_each { |ele| result &= yield ele }
+      my_each { |ele| result &= (yield ele) }
     elsif pattern
       my_each { |ele| result &= ele.equal?(pattern) }
     else
@@ -30,7 +30,18 @@ module Enumerable
     false
   end
 
-  def my_count; end
+  # count elements matching a condition
+  def my_count(param = nil)
+    total = 0
+    if block_given?
+      my_each { |ele| total += 1 if (yield ele) == true }
+    else
+      return length if param.nil?
+
+      my_each { |ele| total += 1 if ele == param }
+    end
+    total
+  end
 
   # provides an enumeration over items,
   # if a block is given - it will be applied to each element
@@ -38,17 +49,43 @@ module Enumerable
   def my_each
     return to_enum :my_each unless block_given?
 
-    tmp = is_a?(Range) ? to_a : self
+    tmp = is_a?(Enumerable) ? to_a : self
     i = 0
     yield tmp[i] && i += 1 until i.equal?(tmp.length)
     self
   end
 
-  def my_each_with_index; end
+  # use each - but spill ele, index on each iteration
+  def my_each_with_index
+    return to_enum :my_each_with_index unless block_given?
 
-  def my_inject; end
+    i = 0
+    my_each do |ele|
+      yield ele, i
+      i += 1
+    end
+    self
+  end
 
-  def my_map; end
+  def my_inject(param_1 = nil, param_2 = nil)
+    tmp = is_a?(Range) ? to_a : self
+    acc = param_1.nil? || param_1.is_a?(Symbol) ? tmp[0] : param_1
+    tmp[0..-1].my_each { |ele| acc = yield(acc, ele) } if block_given? && param_1
+    tmp[1..-1].my_each { |ele| acc = yield(acc, ele) } if block_given? && !param_1
+    tmp[1..-1].my_each { |ele| acc = acc.send(param_1, ele) } if param_1.is_a?(Symbol)
+    tmp[0..-1].my_each { |ele| acc = acc.send(param_2, ele) } if param_2
+    acc
+  end
+
+  # map elements using a block
+  # return enumerable(self) if no block given
+  def my_map
+    return to_enum :my_map unless block_given?
+
+    out = []
+    my_each { |ele| out << (yield ele) }
+    out
+  end
 
   # check if none of
   # implicitly pass a_block if it should be passed into inner functions
@@ -56,9 +93,16 @@ module Enumerable
     !my_any?(pattern, &a_block)
   end
 
-  def my_select; end
+  # select elements - check over block
+  def my_select
+    return to_enum :my_select unless block_given?
+
+    out = []
+    my_each { |ele| out << ele if (yield ele) }
+    out
+  end
 end
 
-p (1..10).my_none? { |x| x.negative? }
-p (1..10).all? { |k| k.positive? }
-p (1..10).my_all? { |k| k.positive? }
+# p ([1, 2, 3, 4].my_map do |e| e * 2; end)
+
+p [1, 2, 3, 4, 5].my_inject(:+)
