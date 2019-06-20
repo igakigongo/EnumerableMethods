ca456cf194449644c38b0ada833fa0f952c1efd3
Edward Iga Kigongo <igakigongo@gmail.com>
2019-06-20 15:15:41 +0200

    Update stickler.yml and spec files

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,28 +1,28 @@
+# frozen_string_literal: true
+
 require_relative '../enumerable'
 
 describe 'enumerable' do
+  describe '.all?' do
+    describe 'should return true when all items' do
+      it 'are truthy' do
+        arr = ['i', 2, 3.4, true]
+        val = arr.my_all? { |ele| ele }
+        expect(val).to be_truthy
+      end
 
-    describe '.all?' do
-        describe 'should return true when all items' do
-            it 'are truthy' do
-                arr = ['i', 2, 3.4, true]
-                val = arr.my_all? { |ele| ele }
-                expect(val).to be_truthy
-            end
-
-            it 'match a pattern' do
-                arr = ['i', 'am', 'a', 'string']
-                val = arr.my_all?(String)
-                expect(val).to be true
-            end
-        end
-        
-        describe 'should return false when some element' do
-            it 'is falsy' do
-                arr = ['i', 2, 3.4, true, nil]
-                val = arr.my_all? { |ele| ele }
-                expect(val).not_to be_truthy
-            end
-        end
+      it 'match a pattern' do
+        val = %w[i am a string].my_all?(String)
+        expect(val).to be true
+      end
     end
+
+    describe 'should return false when some element' do
+      it 'is falsy' do
+        arr = ['i', 2, 3.4, true, nil]
+        val = arr.my_all? { |ele| ele }
+        expect(val).not_to be_truthy
+      end
+    end
+  end
 end
